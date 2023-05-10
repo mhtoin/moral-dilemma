745664fba7605434575c9174641d1038231f7c61
Miika Oinonen <miika.oinonen@maplet.com>
2023-05-10 14:18:19 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button, Stack } from '@mui/joy'
+import options from './options'
+
+function App() {
+  const [count, setCount] = useState("1")
+  const [decisions, setDecisions] = useState(options)
+
+  const handleChoose = (number) => {
+    setCount(count + '-' + number)
+    
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        
+      </div>
+      <h1>Moral Dilemma</h1>
+      <div className='card'>
+        {decisions[count].text}
+      </div>
+      <div className="card">
+        <Stack direction={'row'} spacing={1} justifyContent={'center'}>
+        <Button onClick={() => handleChoose('1')}>{decisions[count]['options']['1']}</Button>
+        <Button onClick={() => handleChoose('2')}>{decisions[count]['options']['2']}</Button>
+        </Stack>
+        <p>
+          Choose your option
+        </p>
+        <Button onClick={() => setCount('1')}>Reset</Button>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/options.js
@@ -0,0 +1,23 @@
+export default {
+    "1": {
+        "text": "You must choose or die!",
+        "options": {
+            "1": "Some decision",
+            "2": "Some other decision"
+        }
+    },
+    "1-1": {
+        "text": "You chose 1! Now choose another",
+        "options": {
+            "1": "Some other decision",
+            "2": "Some other-other decision"
+        }
+    },
+    "1-2": {
+        "text": "You chose 2! Now choose another",
+        "options": {
+            "1": "Some other second decision",
+            "2": "Some other-other second decision"
+        }
+    }
+}
