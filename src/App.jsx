5d40524fe935e7ea14ddfe001507ee1f9e70947e
Miika Oinonen <miika.oinonen@maplet.com>
2023-05-10 14:21:08 +0300

    fixed link

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://github.com/mhtoin/moral-dilemma" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         
