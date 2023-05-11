d08d133fbb752a847d41db9ea5cb8a6d4e051256
Miika Oinonen <miika.oinonen@maplet.com>
2023-05-11 20:11:54 +0300

    removed typewriter

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button, Divider, Stack, Typography } from '@mui/joy'
 import options from './options'
-import Typewriter from 'typewriter-effect';
 
 function App() {
   const [count, setCount] = useState("1")
   const [decisions, setDecisions] = useState(options)
   const [delay, setDelay] = useState(true)
+  const [lang, setLang] = useState('Finnish')
 
   const handleChoose = (number) => {
     console.log(number, count)
     setCount(count + '-' + number)
-    
+  }
 
+  const handleContinue = () => {
+    let treeParent = count ? parseInt(count.split('-')[0]) + 1 : 1
+    setCount(treeParent.toString())
   }
 
 
@@ -30,28 +33,17 @@ function App() {
       <h1>Moral Dilemma</h1>
       <div className='card'>
         {decisions[count]
-          ? <Typewriter
-            options={{
-              strings: decisions[count].text.split(),
-              autoStart: true,
-              loop: delay,
-              delay: 40,
-              pauseFor: 1000000
-              
-            }}
-          />
+          ? <p>{decisions[count].text}</p>
           : 'Congratulations you... won?'}
       </div>
       <div className="card">
         <Stack direction={'column'} spacing={2} justifyContent={'center'} alignItems={'center'} divider={<Divider orientation="horizontal" >Tai</Divider>}>
-          {decisions[count].options && <Button onClick={() => handleChoose('1')}>{decisions[count]['options']['1']}</Button>}
-         
-          {decisions[count].options && <Button onClick={() => handleChoose('2')}>{decisions[count]['options']['2']}</Button>}
+          {decisions[count]?.options && <Button onClick={() => handleChoose('1')}>{decisions[count]['options']['1']}</Button>}  
+          {decisions[count]?.options && <Button onClick={() => handleChoose('2')}>{decisions[count]['options']['2']}</Button>}
+          {decisions[count] && !decisions[count].options && <Button onClick={handleContinue}>Jatka</Button>}
         </Stack>
-        <p>
-          Choose your option
-        </p>
-        <Button onClick={() => setCount('1')}>Reset</Button>
+        
+        <Button sx={{m: 5}} onClick={() => setCount('1')}>Reset</Button>
       </div>
 
     </>
--- src/options.js
+++ src/options.js
@@ -24,12 +24,35 @@ export default {
         "text": "Potilas riehuu n.15 minuuttia ja murtaa kätensä. \n Pyytää aamulla anteeksi käytöstään."
     },
     "1-1-2": {
-        "text": "Potilas saa paniikkikohtauksen.\n Hän on aamulla hiljainen ja masentuneen oloinen."
+        "text": "Potilas saa paniikkikohtauksen. Illemmalla potilas on alakuloinen ja vetäytynyt. Hän pyytää saada olla yksin. Potilas on yleensä aamuun mennessä taas oma itsensä",
+        "options": {
+            "1": "Anna potilaan olla rauhassa.",
+            "2": "Käy potilaan huoneessa pienin väliajoin."
+        }
+    },
+    "1-1-2-1": {
+        "text": "Potilas on yrittänyt satuttaa itseään yön aikana"
+    },
+    "1-1-2-2": {
+        "text": "Potilaalla on pelkotiloja. Hän kiittää sinua huolenpidosta."
     },
     "1-2-1": {
         "text": "Potilas palaa tunnin kuluttua pikaruokapussin kanssa. \n Aamulla hän valittelee vatsakivuista."
     },
     "1-2-2": {
         "text": "Potilas saa kollegasi päästämän hänet tupakalle.\n Hups! Potilas on karannut."
+    },
+    "2": {
+        "text": "Kollegasi ilmoittaa puhelimensa kadonneen. Potilaalla on historiaa toisten potilaiden tavaroiden ottamisesta. Hän vaikuttaa vahingoniloiselta, mutta kieltää ottaneensa puhelimen",
+        "options": {
+            "1": "Tutki potilaan huone",
+            "2": "Auta hoitajaa käymään läpi tavaransa"
+        }
+    },
+    "2-1": {
+        "text": "Et löydä mitään. Potilas masentuu koska häntä ei uskottu."
+    },
+    "2-2": {
+        "text": "Hoitaja muistaa jättäneensä puhelimen autoonsa."
     }
 }
