03db8d084d1955da9ab052c45fbe203fc34cddc5
Miika Oinonen <miika.oinonen@maplet.com>
2023-05-11 20:18:02 +0300

    ability to continue

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,8 @@ function App() {
       </div>
       <div className="card">
         <Stack direction={'column'} spacing={2} justifyContent={'center'} alignItems={'center'} divider={<Divider orientation="horizontal" >Tai</Divider>}>
-          {decisions[count]?.options && <Button onClick={() => handleChoose('1')}>{decisions[count]['options']['1']}</Button>}  
-          {decisions[count]?.options && <Button onClick={() => handleChoose('2')}>{decisions[count]['options']['2']}</Button>}
+          {decisions[count]?.options && decisions[count]?.options["1"] && <Button onClick={() => handleChoose('1')}>{decisions[count]['options']['1']}</Button>}  
+          {decisions[count]?.options && decisions[count]?.options["2"] && <Button onClick={() => handleChoose('2')}>{decisions[count]['options']['2']}</Button>}
           {decisions[count] && !decisions[count].options && <Button onClick={handleContinue}>Jatka</Button>}
         </Stack>
         
--- src/options.js
+++ src/options.js
@@ -21,7 +21,10 @@ export default {
         }
     },
     "1-1-1": {
-        "text": "Potilas riehuu n.15 minuuttia ja murtaa kätensä. \n Pyytää aamulla anteeksi käytöstään."
+        "text": "Potilas riehuu n.15 minuuttia ja murtaa kätensä. \n Pyytää aamulla anteeksi käytöstään.",
+        "options": {
+            "1": "Jatka"
+        }
     },
     "1-1-2": {
         "text": "Potilas saa paniikkikohtauksen. Illemmalla potilas on alakuloinen ja vetäytynyt. Hän pyytää saada olla yksin. Potilas on yleensä aamuun mennessä taas oma itsensä",
@@ -42,17 +45,17 @@ export default {
     "1-2-2": {
         "text": "Potilas saa kollegasi päästämän hänet tupakalle.\n Hups! Potilas on karannut."
     },
-    "2": {
+    "1-1-1-1": {
         "text": "Kollegasi ilmoittaa puhelimensa kadonneen. Potilaalla on historiaa toisten potilaiden tavaroiden ottamisesta. Hän vaikuttaa vahingoniloiselta, mutta kieltää ottaneensa puhelimen",
         "options": {
             "1": "Tutki potilaan huone",
             "2": "Auta hoitajaa käymään läpi tavaransa"
         }
     },
-    "2-1": {
+    "1-1-1-1-1": {
         "text": "Et löydä mitään. Potilas masentuu koska häntä ei uskottu."
     },
-    "2-2": {
+    "1-1-1-1-2": {
         "text": "Hoitaja muistaa jättäneensä puhelimen autoonsa."
     }
 }
